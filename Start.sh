350c4be1ba9a8406713d17550ecfccddba6a9230
Christian Hiland <hilandchristian112@gmail.com>
2023-04-22 12:32:52 -0400

    Create Start.sh

---
+++ Start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# The Options That The User Will Have.
+echo -e "Here are all the Scripts:\n"
+echo -e "1. Install Script\n2. Server Script\nThose are the scripts that can be downlaoded and run."
+read option
+
+if [ option ==  ]
