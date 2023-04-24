47cfef9250ba9a0aa1d429bb7e213fbaffa9fea5
Christian Hiland <hilandchristian112@gmail.com>
2023-04-24 09:08:13 -0400

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+echo -e "Is this your oprating system? [Y/N]"
+echo -e grep "Operating System" | cut -d ' ' -f5-`
+
 # The Options That The User Will Have.
 echo -e "Here are all the Scripts:\n"
 echo -e "1. Install Script\n2. Server Script\nThose are the scripts that can be downlaoded and run."
 read option
 
-if [ option ==  ]
+if [ option ==  "1"]
+then
+
